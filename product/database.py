3ac4c442d0731b9d512519f027609cf3f2177820
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-09-09 01:29:55 -0700

    31 - making a connection

---
+++ product/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy  import crate_engine, engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABSE_URL = 'sqlite:///./product.db'
+
+engine = create_engine(SQLALCHEMY_DATABSE_URL, connect_args = {
+    "check_same_thread": False
+})
+
+SessionLocal  = sessionmaker(bind.engine, autocommit = False, autoflush = False)
+Base = declarative_base()
