f155a83049080cfbd8b881c59cbb693283374b94
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-09-12 17:33:49 -0700

    lecture 39 HTTP status Code

--- product/main.py
+++ product/main.py
@@ -9,6 +9,7 @@ from database import SessionLocal
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from typing import List 
+from fastapi import status 
 
 app = FastAPI()
 
@@ -47,7 +48,7 @@ def product(id, db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id).first()
     return product
 
-@app.post('/product')
+@app.post('/product', status_code = status.HTTP_201_CREATED)
 def add(request: schemas.Product, db: Session = Depends(get_db) ): 
     new_product = models.Product(name = request.name, description = request.description, price = request.price)
     db.add(new_product)
