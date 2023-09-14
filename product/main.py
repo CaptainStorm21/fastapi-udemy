45152435140b48b419fa2289695f647eebe1c60b
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-09-14 03:15:29 -0700

    lecture 46 api router

--- product/main.py
+++ product/main.py
@@ -1,16 +1,16 @@
+
+from .database import engine
 from  fastapi import FastAPI, Response, HTTPException
-#from .import schemas
-#from .import models
-#from .database import engine
-import models
-from database import engine
-import schemas
-from database import SessionLocal
+from sqlalchemy.sql.functions import mode
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
-from typing import List 
-from fastapi import status 
+from starlette.status import HTTP_404_NOT_FOUND
+from .database import engine, SessionLocal
+from fastapi import status
 from passlib.context import CryptContext
+from .routers import product
+from .import schemas
+from .import models
 
 pwd_context = CryptContext(schemes = ["bcrypt"], deprecated="auto")
 
@@ -24,50 +24,9 @@ app = FastAPI(
     }
 )
 
+app.include_router(product.router)
 models.Base.metadata.create_all(engine)
 
-def get_db():
-    db = SessionLocal()
-    try: 
-        yield db
-    finally: 
-        db.close()
-
-@app.delete('/product/{id}', tags = ['products to be deleted'])
-def delete(id, db: Session = Depends(get_db)):
-    db.query(models.Product).filter(models.Product.id == id).delete(synchronize_session=False)
-    db.commit()
-    return {'Entry Deleted'}
-
-@app.put('/product/{id}', tags = ['update products'])
-def update(id, request: schemas.Product, db: Session = Depends(get_db)):
-    product = db.query(models.Product).filter(models.Product.id == id)
-    if not product.first():
-        pass 
-    product.update(request.dict())
-    db.commit()
-    return {'Product was updated'
-}
-
-@app.get('/products', response_model= List[schemas.DisplayProduct], tags=['get a list of all products'])
-def products(db: Session = Depends(get_db)):
-    products = db.query(models.Product).all()
-    return products
-
-@app.get('/product/{id}', response_model=schemas.DisplayProduct)
-def product(id, response: Response, db: Session = Depends(get_db)):
-    product = db.query(models.Product).filter(models.Product.id == id).first()
-    if not product:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = 'Product not found')
-    return product
-
-@app.post('/product', status_code = status.HTTP_201_CREATED)
-def add(request: schemas.Product, db: Session = Depends(get_db) ): 
-    new_product = models.Product(name = request.name, description = request.description, price = request.price, seller_id=1)
-    db.add(new_product)
-    db.commit()
-    db.refresh(new_product)
-    return request
 
 @app.post('/seller', response_model=schemas.DisplaySeller)
 def create_seller(request:schemas.Seller,  db: Session = Depends(get_db)):
---
+++ product/routers/product.py
@@ -0,0 +1,46 @@
+from  fastapi import APIRouter
+from fastapi import  Response, HTTPException, FastAPI
+from sqlalchemy.orm import Session
+from fastapi.params import Depends
+from ..database import get_db
+from ..import models, schemas
+from typing import List
+from fastapi import status 
+
+router = APIRouter()
+
+@router.delete('/product/{id}', tags = ['products to be deleted'])
+def delete(id, db: Session = Depends(get_db)):
+    db.query(models.Product).filter(models.Product.id == id).delete(synchronize_session=False)
+    db.commit()
+    return {'Entry Deleted'}
+
+@router.put('/product/{id}', tags = ['update products'])
+def update(id, request: schemas.Product, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == id)
+    if not product.first():
+        pass 
+    product.update(request.dict())
+    db.commit()
+    return {'Product was updated'
+}
+
+@router.get('/products', response_model= List[schemas.DisplayProduct], tags=['get a list of all products'])
+def products(db: Session = Depends(get_db)):
+    products = db.query(models.Product).all()
+    return products
+
+@router.get('/product/{id}', response_model=schemas.DisplayProduct)
+def product(id, response: Response, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == id).first()
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = 'Product not found')
+    return product
+
+@router.post('/product', status_code = status.HTTP_201_CREATED)
+def add(request: schemas.Product, db: Session = Depends(get_db) ): 
+    new_product = models.Product(name = request.name, description = request.description, price = request.price, seller_id=1)
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return request
